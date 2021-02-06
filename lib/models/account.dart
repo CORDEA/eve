d3e6ad8923b90d26efe2b916e372b74e3616a5d0
Yoshihiro Tanaka <contact@cordea.jp>
2021-02-06 17:38:32 +0900

    define model

---
+++ lib/models/account.dart
@@ -0,0 +1,8 @@
+class Account {
+  final String number;
+  final String ownerName;
+  final String branchCode;
+  final String branchName;
+
+  const Account(this.number, this.ownerName, this.branchCode, this.branchName);
+}
