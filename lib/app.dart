4ce507bf2229e3eef8a6111477234f9cc8d3f286
Yoshihiro Tanaka <contact@cordea.jp>
2021-02-07 15:43:18 +0900

    provide repository

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:eve/repositories/account_repository.dart';
+import 'package:eve/repositories/notification_repository.dart';
 import 'package:eve/ui/base.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -6,8 +7,11 @@ import 'package:provider/provider.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Provider(
-      create: (_) => AccountRepository(),
+    return MultiProvider(
+      providers: [
+        Provider(create: (_) => AccountRepository()),
+        Provider(create: (_) => NotificationRepository()),
+      ],
       child: MaterialApp(
         title: 'eve',
         theme: ThemeData(
