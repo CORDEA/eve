2b8711dfd4b4e9ab6c187c2ff4684578680d50c8
Yoshihiro Tanaka <contact@cordea.jp>
2021-02-08 16:20:58 +0900

    display items

--- lib/ui/details.dart
+++ lib/ui/details.dart
@@ -1,4 +1,5 @@
 import 'package:eve/bloc/details_bloc.dart';
+import 'package:eve/models/detail.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -26,13 +27,28 @@ class _DetailsContent extends State<_Details> {
 
   @override
   Widget build(BuildContext context) {
-    return Center();
+    var length = context.select<DetailsBloc, int>(
+      (value) => value.details.length,
+    );
+    return ListView.builder(
+      itemCount: length,
+      itemBuilder: (_, index) => _DetailsItem(index: index),
+    );
   }
 }
 
 class _DetailsItem extends StatelessWidget {
+  final int _index;
+
+  _DetailsItem({Key key, @required index})
+      : _index = index,
+        super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    var item = context.select<DetailsBloc, Detail>(
+      (value) => value.details[_index],
+    );
     return Container(
       constraints: BoxConstraints(
         minHeight: 72,
@@ -46,7 +62,7 @@ class _DetailsItem extends StatelessWidget {
             children: [
               Expanded(
                 child: Text(
-                  '',
+                  item.title,
                   style: Theme.of(context).textTheme.subtitle1,
                   maxLines: 1,
                   overflow: TextOverflow.ellipsis,
