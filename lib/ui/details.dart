47763834cff0d939ff62c9f16f7b5de15ee32328
Yoshihiro Tanaka <contact@cordea.jp>
2021-02-08 16:18:09 +0900

    define item

--- lib/ui/details.dart
+++ lib/ui/details.dart
@@ -29,3 +29,45 @@ class _DetailsContent extends State<_Details> {
     return Center();
   }
 }
+
+class _DetailsItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints(
+        minHeight: 72,
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Text(
+                  '',
+                  style: Theme.of(context).textTheme.subtitle1,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              const SizedBox(width: 8),
+              Text(
+                '',
+                style: Theme.of(context).textTheme.caption,
+              ),
+            ],
+          ),
+          const SizedBox(height: 4),
+          Text(
+            '',
+            style: Theme.of(context).textTheme.caption,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ],
+      ),
+    );
+  }
+}
