7a4db4c56ffef06d35ab279b403827d37e5e0d3c
Yoshihiro Tanaka <contact@cordea.jp>
2021-02-09 16:33:36 +0900

    update layout

--- lib/ui/transfer.dart
+++ lib/ui/transfer.dart
@@ -3,6 +3,40 @@ import 'package:flutter/material.dart';
 class Transfer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center();
+    return ListView.builder(
+      itemCount: 2,
+      itemBuilder: (context, index) {
+        switch (index) {
+          case 0:
+            return _TransferSectionLabel(text: 'Starred');
+          case 1:
+            return _TransferSectionLabel(text: 'Past recipients');
+          default:
+            return Center();
+        }
+      },
+    );
+  }
+}
+
+class _TransferSectionLabel extends StatelessWidget {
+  final String text;
+
+  const _TransferSectionLabel({Key key, this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 24,
+        top: 16,
+        right: 24,
+        bottom: 16,
+      ),
+      child: Text(
+        text,
+        style: Theme.of(context).textTheme.headline5,
+      ),
+    );
   }
 }
