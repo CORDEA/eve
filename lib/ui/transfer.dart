36eaed512f44ed1d2d814c0b385e4559ec9310b5
Yoshihiro Tanaka <contact@cordea.jp>
2021-02-12 17:05:18 +0900

    update layout

--- lib/ui/transfer.dart
+++ lib/ui/transfer.dart
@@ -5,7 +5,93 @@ class Transfer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Transfer')),
-      body: Center(),
+      body: ListView(
+        padding: EdgeInsets.all(16),
+        children: [
+          const SizedBox(height: 16),
+          _TransferSourceItem(),
+          const SizedBox(height: 16),
+          const Icon(Icons.arrow_downward),
+          const SizedBox(height: 16),
+          _TransferDestinationItem(),
+          const SizedBox(height: 44),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Theme(
+              data: Theme.of(context).copyWith(
+                primaryColor: Theme.of(context).accentColor,
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  prefixText: '¥',
+                ),
+                cursorColor: Theme.of(context).accentColor,
+                style: Theme.of(context).textTheme.headline5,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _TransferSourceItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '',
+              style: Theme.of(context).textTheme.caption,
+            ),
+            const SizedBox(height: 4),
+            Text(
+              '',
+              style: Theme.of(context).textTheme.caption.copyWith(
+                    color: Theme.of(context).textTheme.subtitle1.color,
+                  ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _TransferDestinationItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '',
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+            const SizedBox(height: 12),
+            Text(
+              '',
+              style: Theme.of(context).textTheme.caption,
+            ),
+            const SizedBox(height: 4),
+            Text(
+              '',
+              style: Theme.of(context).textTheme.caption.copyWith(
+                    color: Theme.of(context).textTheme.subtitle1.color,
+                  ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
